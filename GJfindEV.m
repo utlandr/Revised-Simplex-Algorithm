84d2b53ba17040a3b02f54233b55de0bba94e758
Reed Bell <rbell11235@hotmail.co.nz>
2018-04-23 13:41:04 +1200

    Altered the calculation for reduced costs to use loops rather than matrix/vector operations. The (much faster) original method is commented for future reference.

--- GJfindEV.m
+++ GJfindEV.m
@@ -20,8 +20,33 @@ function [s, minrc] =   GJfindEV(m,n, c, A, varstatus,pi)
     %Form N from constraint matrix 
     N = A(:,varstatus == 0); 
     
-    %Compute vector of reduced costs for non-basic variables. 
-    redCost = transpose(c(varstatus==0)) - transpose(pi)*N;
+    %Compute vector of reduced costs for non-basic variables. This is
+    %looped for proof of understanding. However, the matrix operation below
+    %is much more efficient:
+    %=======================================================
+    %redCost = transpose(c(varstatus==0)) - transpose(pi)*N;
+    %=======================================================
+    
+    %Pre-allocation
+    cN = c(varstatus==0);
+    piT = transpose(pi);
+    redCost = zeros(1,m);
+    
+    %Iterate through and calculate reduced cost for each variable (and form
+    %reduced costs vector
+    for i=1:n-m
+        
+        %Perform matrix multiplication to get piTN for each column
+        piTN = 0;
+        for j = 1:m
+            piTN = piTN + piT(j)*N(j,i); 
+        
+        end
+        
+        %Calculate reduced cost for variable (cN - piT*N)
+        redCost(i) = cN(i) - piTN;
+        
+    end
     
     %Find the index of the variable (x_s) that will reduce the objective function the
     %most. If no variables decrease the objective function, then the current
@@ -33,4 +58,5 @@ function [s, minrc] =   GJfindEV(m,n, c, A, varstatus,pi)
         s = 0;
     
     end
+    
 end
