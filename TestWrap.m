72366ad0b170160bd3e225e6665844ade66a2f0c
Reed Bell <rbell11235@hotmail.co.nz>
2018-04-26 00:22:21 +1200

    Definitions and playing with initialisation

--- TestWrap.m
+++ TestWrap.m
@@ -1,18 +1,29 @@
-function    []  =   TestWrap(mat)
+function    []  =   TestWrap(testDir,rsmDir,testTypes)
 %   TestWrap.m is a wrapper script that tests various stages of the Revised
-%   Simplex algorithm implementation using preworked problems. It parses a
-%   .mat file containing the necessary data for inputting into GJrsm().
+%   Simplex algorithm implementation using preworked problems. Assumes
+%   Assignment1 naming scheme for 1c
 %
 %   Inputs:
-%       mat         =       .mat files containing necessay data for test
-%                           conditions.
+%       testDir         =       Directory string (can be relative or absolute)
+%                               containing test case (.m) files. Default is
+%                               './test-cases/'
+%
+%       rsmDir          =       Directory string to RSM algorithm wrapper
+%                               script 'GJrsm or fullrsm'. Default is CWD.
+%
+%       testTypes       =       vector of strings specifying specific tests
+%                               or test files to be used. 
 %
 %   Outputs:
 %       
 %   Author:
 %       Reed Bell   -       rbel068@aucklanduni.ac.nz
 
-%Load .mat file containing test cases
-load(mat)
+%Default input values would be nice, but can't be stuffed atm to user
+%matlabs' parser (what appears to be much uglier than python's argparser).
+%So, for now, we deal with putting the right value in
 
-%Perform the Revised Simplex Method
+%Set working directory to where the RSM implementation is located
+cd(rsmDir)
+
+%Grab required .mat files
