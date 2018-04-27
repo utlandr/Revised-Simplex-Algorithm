c70813775d82a1094c159de6cde0eb648feb03a7
Reed Bell <rbel068@aucklanduni.ac.nz>
2018-04-27 12:57:38 +1200

    New test cases

--- wrapper.m
+++ wrapper.m
@@ -1,15 +1,17 @@
 %Wrapper script that iterates through folder containing test files and
 %performs the RSM algorithm on it.
-
+clear
 testFiles = dir('**/test-cases/test*');
 iter = struct2cell(testFiles);
 iter = iter([1,2,],:);
-%Clear and load new 
+
+%Iterate through test cases 
 for iter = iter
     
     %Clear space of function input variables
     clearvars -except testFiles iter
-    load(string(strcat(iter(2),'/',iter(1))))
+    load(char(strcat(iter(2),'/',iter(1))))
+    disp(iter(1))
     
     %Call function (trying)
     try
